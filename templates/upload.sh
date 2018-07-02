6a96fb2944039f7dbf6c78415c7f45b63a01e117
huzhihui <wonderfulhzh@163.com>
2018-07-02 14:58:35 +0800

    1.0

--- templates/upload.sh
+++ templates/upload.sh
@@ -1,21 +1,53 @@
 #!/bin/bash
 
-git stash
-git pull origin master --tags
-git stash pop
+Cyan='\033[0;36m'
+Default='\033[0;m'
 
-VersionString=`grep -E 's.version.*=' __ProjectName__.podspec`
-VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+versionNum=""
+privatePodsName=""
+confirmed="n"
 
-NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' __ProjectName__.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" __ProjectName__.podspec
+getVersionNum(){
+    read -p "Enter VersionNum:" versionNum
 
-echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+    if test -z "$versionNum";then
+        getVersionNum
+    fi
+}
 
-git add .
-git commit -am ${NewVersionNumber}
-git tag ${NewVersionNumber}
-git push origin master --tags
-cd ~/.cocoapods/repos/PrivatePods && git pull origin master && cd - && pod repo push PrivatePods __ProjectName__.podspec --verbose --allow-warnings --use-libraries
+getPrivatePodsName(){
+    read -p "Enter privatePodsName:" privatePodsName
+
+    if test -z "$privatePodsName";then
+        getPrivatePodsName
+    fi
+}
+
+getInfomation(){
+    getVersionNum
+    getPrivatePodsName
+
+
+    echo -e "============================================"
+    echo -e "versionNum       : ${Cyan}${versionNum}${Default}"
+    echo -e "privatePodsName  : ${Cyan}${privatePodsName}${Default}"
+    echo -e "============================================"
+}
+
+echo -e "\n"
+while [ "$confirmed" != "y" -a "$confirmed" != "Y" ]
+do
+    if [ "$confirmed" == "n" -o "$confirmed" == "N" ]; then
+       getInfomation
+    fi
+    read -p "confirm? (y/n):" confirmed
+done
+
+
+
+git add .  &> /dev/null
+git commit -m ${versionNum}  &> /dev/null
+git tag ${versionNum}  &> /dev/null
+git push origin master --tags  &> /dev/null
+cd ~/.cocoapods/repos/${privatePodsName} && git pull origin master && cd - && pod repo push ${privatePodsName} __ProjectName__.podspec --verbose --allow-warnings --use-libraries  &> /dev/null
 
